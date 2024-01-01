3fe3a3551d46fdee65db8b49e0acfa238011a0ae
HMThang <87440494+HMThang@users.noreply.github.com>
2024-01-01 23:15:00 +0700

    Update TowersofHanoi.java

--- DSA/src/Lab2/TowersofHanoi.java
+++ DSA/src/Lab2/TowersofHanoi.java
@@ -1,4 +1,17 @@
 package Lab2;
 
 public class TowersofHanoi {
+   public static void moveDisk(int disk, char source, char dest, char spare) {
+        if (disk == 0) {
+            System.out.println("Move disk " + disk + " from rod " + source + " to rod " + dest);
+        } else {
+            moveTower(disk - 1, source, spare, dest); // Bước 1
+            System.out.println("Move disk " + disk + " from rod " + source + " to rod " + dest); // Bước 2
+            moveTower(disk - 1, spare, dest, source); // Bước 3
+        }
+    }
+
+    public static void moveTower(int disk, char source, char dest, char spare) {
+        moveDisk(disk, source, dest, spare);
+    }
 }
